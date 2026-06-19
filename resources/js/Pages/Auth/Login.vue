24c94af50261d3ea7c6adb176cf8d22744398816
Dimas <dimasagung733@gmail.com>
2026-06-19 22:58:09 +0700

    feat: add show/hide password toggle (eye icon) on login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -9,6 +9,7 @@ const form = useForm({
 });
 
 const isLoading = ref(false);
+const showPassword = ref(false);
 
 const submit = () => {
   isLoading.value = true;
@@ -114,15 +115,31 @@ const submit = () => {
                 </svg>
               </span>
               <input 
-                type="password" 
+                :type="showPassword ? 'text' : 'password'" 
                 id="password" 
                 v-model="form.password" 
                 required
                 autocomplete="current-password"
                 placeholder="Masukkan kata sandi"
-                class="w-full border border-gray-200 rounded-lg p-2.5 pl-10 text-sm focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500 outline-none transition-all"
+                class="w-full border border-gray-200 rounded-lg p-2.5 pl-10 pr-10 text-sm focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500 outline-none transition-all"
                 :class="form.errors.password ? 'border-red-300 bg-red-50' : ''"
               />
+              <button
+                type="button"
+                @click="showPassword = !showPassword"
+                class="absolute inset-y-0 right-0 pr-3.5 flex items-center text-gray-400 hover:text-emerald-600 transition-colors"
+                :title="showPassword ? 'Sembunyikan kata sandi' : 'Tampilkan kata sandi'"
+              >
+                <!-- Eye open -->
+                <svg v-if="!showPassword" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                </svg>
+                <!-- Eye closed -->
+                <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
+                </svg>
+              </button>
             </div>
             <span v-if="form.errors.password" class="text-xs text-red-600 font-medium">{{ form.errors.password }}</span>
           </div>

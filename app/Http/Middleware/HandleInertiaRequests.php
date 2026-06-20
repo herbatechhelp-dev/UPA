198e9a66939276f45e53ff0f911f0230344c1cb7
Dimas <dimasagung733@gmail.com>
2026-06-20 19:28:51 +0700

    fix: prevent browser from caching Inertia JSON responses

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,11 +4,28 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Symfony\Component\HttpFoundation\Response;
 
 class HandleInertiaRequests extends Middleware
 {
+    /**
+     * Handle the incoming request.
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = parent::handle($request, $next);
+
+        if ($request->hasHeader('X-Inertia')) {
+            $response->headers->set('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
+            $response->headers->set('Pragma', 'no-cache');
+            $response->headers->set('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
+        }
+
+        return $response;
+    }
     /**
      * The root template that is loaded on the first page visit.
      *
